2ba4cf5dbaaf91cb3d605b7de82da4d79b16683c
Elijah Cobb <elijah@elijahcobb.com>
2020-06-01 16:57:21 -0400

    redoing commands

--- ts/commands.ts
+++ ts/commands.ts
@@ -6,137 +6,14 @@
  */
 
 import {KBCommand, KBMessage, KBResponse} from "@elijahjcobb/keybase-bot-builder";
-import {Covid19, Country, Global, Report} from "@elijahjcobb/covid19";
-
-function getMessageForCountry(country: Country): string {
-
-	return `${country.country} has *${country.cases.toLocaleString()} total cases*. There were ${country.todayCases.toLocaleString()} new cases today and sadly ${country.todayDeaths.toLocaleString()} people lost their lives today. ${country.critical.toLocaleString()} people are in critical condition right now. There are *${country.active.toLocaleString()} active cases*, *${country.deaths.toLocaleString()} deaths*, and thankfully *${country.recovered.toLocaleString()} recoveries*. Out of one million people ${country.casesPerOneMillion.toLocaleString()} have the virus (\\~${((country.casesPerOneMillion / 1_000_000) * 100).toFixed(2)}%) and ${country.deathsPerOneMillion.toLocaleString()} and (\\~${((country.deathsPerOneMillion / country.casesPerOneMillion) * 100).toFixed(2)}%) of cases have died.`;
-
-}
-
-function getMessageForGlobal(global: Global): string {
-
-	return `Throughout the world, the *total number of cases is ${global.cases.toLocaleString()}*. Sadly ${global.deaths.toLocaleString()} people have lost their life but thankfully ${global.recovered.toLocaleString()} have recovered.`;
-
-}
+import {Covid19} from "@elijahjcobb/covid19";
 
 export const commands: KBCommand[] = [
 	{
-		name: "global",
-		description: "Get current global statistics.",
+		name: "",
+		description: "",
 		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			const global: Global = await Covid19.getGlobal();
-			await res.send(getMessageForGlobal(global));
-
+			
 		}
-	},
-	{
-		name: "usa",
-		description: "Get current statistics for the United States",
-		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			const country: Country = await Covid19.getUnitedStates();
-			await res.send(getMessageForCountry(country));
-
-		}
-	},
-	{
-		name: "country",
-		description: "Get current statistics for any country.",
-		usage: "!country China",
-		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			const params: (string | number)[] = msg.getParameters();
-			const param: string | number = params[0];
-			if (typeof param === "number") return await res.send("*Whoops!* You must provide a country. Try `!country China`");
-
-			const country: Country = await Covid19.getCountry(param);
-			await res.send(getMessageForCountry(country));
-
-		}
-	},
-	{
-		name: "deaths",
-		description: "The total amount of deaths world wide.",
-		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			const global: Global = await Covid19.getGlobal();
-			await res.send(global.deaths);
-
-		}
-	},
-	{
-		name: "cases",
-		description: "The total amount of cases world wide.",
-		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			const global: Global = await Covid19.getGlobal();
-			await res.send(global.cases);
-
-		}
-	},
-	{
-		name: "recovered",
-		description: "The total amount of recoveries world wide.",
-		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			const global: Global = await Covid19.getGlobal();
-			await res.send(global.recovered);
-
-		}
-	},
-	{
-		name: "top",
-		description: "The country that has the highest number of cases.",
-		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			const countries: Country[] = await Covid19.getAllCountries();
-			countries.sort((a: Country, b: Country): number => { return b.cases - a.cases; });
-
-			// using index 1 because index 0 will always be the 'world' entry the api provides
-			const country: Country | undefined = countries[1];
-
-			if (country === undefined) return await res.send("Whoops! There is an API error. Try again later.");
-
-			await res.send(getMessageForCountry(country));
-
-		}
-	},
-	{
-		name: "rank",
-		description: "The ranking of countries and cases. Provide a number to limit result count.",
-		usage: "!rank 10",
-		handler: async (msg: KBMessage, res: KBResponse): Promise<void> => {
-
-			let rank: string | number = msg.getParameters()[0];
-			if (typeof rank === "string") rank = 10000;
-
-			const countries: Country[] = await Covid19.getAllCountries();
-
-			countries.sort((a: Country, b: Country): number => {
-
-				return b.cases - a.cases;
-
-			});
-
-			let message: string = "";
-			let i: number = 1;
-
-			for (const country of countries) {
-
-				// The api being used now returns "world" info in this endpoint.
-				if (country.country === "World") continue;
-
-				message += `*${i}*. ${country.country} _(${country.cases})_\n`;
-				i++;
-
-				if (i > rank) break;
-
-			}
-
-			await res.send(message);
-
-		}
-	},
+	}
 ];
--- ts/index.ts
+++ ts/index.ts
@@ -7,7 +7,7 @@
 
 import * as FS from "fs";
 import * as Path from "path";
-import {KBBot} from "@elijahjcobb/keybase-bot-builder";
+import {KBBot, KBConversation, KBResponse} from "@elijahjcobb/keybase-bot-builder";
 import {commands} from "./commands";
 
 (async(): Promise<void> => {
@@ -18,11 +18,16 @@ import {commands} from "./commands";
 
 	const bot: KBBot = await KBBot.init("covid19_stats", paperKey, {
 		debugging: false,
-		logging: true,
-		checkAllMessages: false
+		logging: true
 	});
 
 	for (const command of commands) bot.command(command);
+
+	bot.onNewConversation(async(conv: KBConversation, res: KBResponse): Promise<void> => {
+
+		await res.send(`Hello! If you notice any problems with me let @elijahcobb know. My data is from the John Hopkins CSSE and is made available by covid19api.com and accessible from the NPM package \`@elijahjcobb/covid19\`. Use a \`!\` to see my commands.`);
+
+	});
 
 	bot.start();
 
